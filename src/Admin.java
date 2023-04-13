cf63176f53b570e64c8e6ebe5457f692fc45ce5f
Samin <samin.mhr2004@gmail.com>
2023-04-13 14:09:27 +0430

    admin menu added

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,40 @@
+import appearance.Appearance;
+import java.util.Scanner;
 public class Admin {
+    Scanner input = new Scanner(System.in);
+    private final Template TEMPLATE;
+    public Admin(Template TEMPLATE) {
+        this.TEMPLATE = TEMPLATE;
+    }
+    // this function is for handling admin menu
+    public void adminMenu() {
+        System.out.println(Appearance.BLUE + "[ ADMIN MENU OPTIONS ]" + Appearance.RESET_COLOR);
+        System.out.println(Appearance.TEXT_ITALIC +  """
+            1. Add
+            2. Update
+            3. Remove
+            4. Flight schedules
+            0. Sign out
+            Choose :\s""" +  Appearance.RESET_STYLE);
+
+        String choice = input.next();
+        label :
+        while (true) {
+            switch (TEMPLATE.availableInput(choice)) {
+                case "1" :
+                    break label;
+                case "2" :
+                    break label;
+                case "3" :
+                    break label;
+                case "4" :
+                    break label;
+                case "0" :
+                    break label;
+                default :
+                    System.out.println(Appearance.RED + "Wrong command! Try again :" + Appearance.RESET_COLOR);
+                    choice = input.next();
+            }
+        }
+    }
 }
