d6285f5e4c5cb76e59d25e92792f2d56bf0a01cb
Samin <samin.mhr2004@gmail.com>
2023-04-13 14:12:01 +0430

    passenger menu added

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,2 +1,46 @@
+import appearance.Appearance;
+import java.util.Scanner;
 public class Passenger {
+    Scanner input = new Scanner(System.in);
+    private final Template TEMPLATE;
+    public Passenger(Template TEMPLATE) {
+        this.TEMPLATE = TEMPLATE;
+    }
+    public void passengerMenu() {
+
+        System.out.println(Appearance.BLUE + "[ PASSENGER MENU OPTIONS ]" + Appearance.RESET_COLOR);
+        System.out.println(Appearance.TEXT_ITALIC + """
+            1. Change password
+            2. Search flight ticket
+            3. Booking ticket
+            4. ticket cancellation
+            5. Booked ticket
+            6. Add Charge
+            0. Sign out
+            Choose :\s""" + Appearance.RESET_STYLE);
+
+        String choice = input.next();
+        label :
+        while (true) {
+            switch (TEMPLATE.availableInput(choice)) {
+                case "1":
+                    break label;
+                case "2":
+                    break label;
+                case "3":
+                    break label;
+                case "4":
+                    break label;
+                case "5":
+                    break label;
+                case "6":
+                    break label;
+                case "0":
+                    break label;
+                default:
+                    System.out.println(Appearance.RED + "Wrong command! Try again :" + Appearance.RESET_COLOR);
+                    choice = input.next();
+            }
+        }
+    }
 }
